415671b1c3a20de489547499b8f4eb8f9514e8b4
Mr.Metelus <mdm@blue42.home>
2019-05-14 19:33:41 -0400

    ...

--- src/server.c
+++ src/server.c
@@ -66,7 +66,7 @@ int send_response(int fd, char *header, char *content_type, void *body, int cont
                                             "\n",
                                   header, asctime(gmtime(&date_time)), content_length, content_type);
     // Send it all!
-    // int rv = send(fd, response, response_length, 0);
+    
     memcpy(response + response_length, body, content_length);
     int rv = send(fd, response, response_length + content_length, 0);
 
